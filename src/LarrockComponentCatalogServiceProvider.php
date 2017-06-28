a4d31c1f94ca7cd890cf09d1fa766118d7268303
Alexandr Fanamurov <fanamura@gmail.com>
2017-06-28 13:24:12 +1000

    Fix migrations

--- src/LarrockComponentCatalogServiceProvider.php
+++ src/LarrockComponentCatalogServiceProvider.php
@@ -33,29 +33,27 @@ class LarrockComponentCatalogServiceProvider extends ServiceProvider
         $timestamp = date('Y_m_d_His', time());
         $migrations = [];
         if ( !class_exists('CreateCatalogTable')){
-            $migrations = [__DIR__.'/../database/migrations/0000_00_00_000000_create_catalog_table.php' => database_path('migrations/'.$timestamp.'_create_catalog_table.php')];
+            $migrations[__DIR__.'/../database/migrations/0000_00_00_000000_create_catalog_table.php'] = database_path('migrations/'.$timestamp.'_create_catalog_table.php');
         }
         if ( !class_exists('CreateCategoryCatalogTable')){
-            $migrations = [__DIR__.'/../database/migrations/0000_00_00_000000_create_category_catalog_table.php' => database_path('migrations/'.$timestamp.'_create_category_catalog_table.php')];
+            $migrations[__DIR__.'/../database/migrations/0000_00_00_000000_create_category_catalog_table.php'] = database_path('migrations/'.$timestamp.'_create_category_catalog_table.php');
         }
         if ( !class_exists('CreateOptionParamTable')){
-            $migrations = [__DIR__.'/../database/migrations/0000_00_00_000000_create_option_param_table.php' => database_path('migrations/'.$timestamp.'_create_option_param_table.php')];
+            $migrations[__DIR__.'/../database/migrations/0000_00_00_000000_create_option_param_table.php'] = database_path('migrations/'.$timestamp.'_create_option_param_table.php');
         }
         if ( !class_exists('CreateOptionParamLinkTable')){
-            $migrations = [__DIR__.'/../database/migrations/0000_00_00_000000_create_option_param_link_table.php' => database_path('migrations/'.$timestamp.'_create_option_param_link_table.php')];
+            $migrations[__DIR__.'/../database/migrations/0000_00_00_000000_create_option_param_link_table.php'] = database_path('migrations/'.$timestamp.'_create_option_param_link_table.php');
         }
         if ( !class_exists('AddForeignKeysToCatalogTable')){
-            $migrations = [__DIR__.'/../database/migrations/0000_00_00_000000_add_foreign_keys_to_catalog_table.php' => database_path('migrations/'.$timestamp.'_add_foreign_keys_to_catalog_table.php')];
+            $migrations[__DIR__.'/../database/migrations/0000_00_00_000000_add_foreign_keys_to_catalog_table.php'] = database_path('migrations/'.$timestamp.'_add_foreign_keys_to_catalog_table.php');
         }
         if ( !class_exists('AddForeignKeysToCategoryCatalogTable')){
-            $migrations = [__DIR__.'/../database/migrations/0000_00_00_000000_add_foreign_keys_to_category_catalog_table.php' => database_path('migrations/'.$timestamp.'_add_foreign_keys_to_category_catalog_table.php')];
+            $migrations[__DIR__.'/../database/migrations/0000_00_00_000000_add_foreign_keys_to_category_catalog_table.php'] = database_path('migrations/'.$timestamp.'_add_foreign_keys_to_category_catalog_table.php');
         }
         if ( !class_exists('AddForeignKeysToOptionParamLinkTable')){
-            $migrations = [__DIR__.'/../database/migrations/0000_00_00_000000_add_foreign_keys_to_option_param_link_table.php' => database_path('migrations/'.$timestamp.'_add_foreign_keys_to_option_param_link_table')];
+            $migrations[__DIR__.'/../database/migrations/0000_00_00_000000_add_foreign_keys_to_option_param_link_table.php'] = database_path('migrations/'.$timestamp.'_add_foreign_keys_to_option_param_link_table');
         }
 
-        $this->publishes([
-            $migrations
-        ], 'migrations');
+        $this->publishes($migrations, 'migrations');
     }
 }
