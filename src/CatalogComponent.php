c57cec355b5d744b491b4729ada2442ecf756be0
Alexandr Fanamurov <fanamura@gmail.com>
2017-10-17 18:05:26 +1000

    add row description_link to admin

--- src/CatalogComponent.php
+++ src/CatalogComponent.php
@@ -74,6 +74,9 @@ class CatalogComponent extends Component
         $row = new FormInput('articul', 'Артикул');
         $this->rows['articul'] = $row->setCssClassGroup('uk-width-1-2 uk-width-medium-1-3 uk-width-large-1-4')->setTemplate('in_card');
 
+        $row = new FormInput('description_link', 'ID материала Feed для описания');
+        $this->rows['description_link'] = $row->setCssClassGroup('uk-width-1-2 uk-width-medium-1-3 uk-width-large-1-4');
+
         /*$row = new FormCheckbox('label_new', 'Метка нового');
         $this->rows['label_new'] = $row->setCssClassGroup('uk-width-1-2 uk-width-medium-1-3 uk-width-large-1-4');
 
