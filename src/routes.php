e89676114c16d75cabda2b972e12c0657318a773
Alexandr Fanamurov <fanamura@gmail.com>
2017-11-22 16:37:47 +1000

    fix

--- src/routes.php
+++ src/routes.php
@@ -1,28 +1,15 @@
 <?php
 
-$middlewares = ['web', 'GetSeo'];
-if(file_exists(base_path(). '/vendor/fanamurov/larrock-menu')){
-    $middlewares[] = 'AddMenuFront';
-}
-if(file_exists(base_path(). '/vendor/fanamurov/larrock-blocks')){
-    $middlewares[] = 'AddBlocksTemplate';
-}
-if(file_exists(base_path(). '/vendor/fanamurov/larrock-discount')){
-    $middlewares[] = 'DiscountsShare';
-}
-
-Route::group(['middleware' => $middlewares], function(){
-    Route::get('/catalog', function(){ return Redirect::to('/'); });
-    Route::get('/root', 'Larrock\ComponentCatalog\CatalogController@getCategoryRoot')->name('catalog.root');
-    Route::get('/yml.xml', 'Larrock\ComponentCatalog\CatalogController@YML')->name('catalog.yml');
-    Route::get('/catalog/all', 'Larrock\ComponentCatalog\CatalogController@getAllTovars')->name('catalog.all');
-    Route::get('/catalog/{category}/{category2?}/{category3?}/{category4?}', 'Larrock\ComponentCatalog\CatalogController@getCategoryExpanded')->name('catalog.category');
-    Route::any('/search/catalog/serp/{words?}', 'Larrock\ComponentCatalog\CatalogController@searchResult')->name('catalog.search.words');
-    Route::get('/search/catalog', 'Larrock\ComponentCatalog\CatalogController@searchItem')->name('catalog.search');
-    Route::post('/ajax/editPerPage', 'Larrock\ComponentCatalog\CatalogController@editPerPage')->name('catalog.editPerPage');
-    Route::post('/ajax/sort', 'Larrock\ComponentCatalog\CatalogController@sort')->name('catalog.sort');
-    Route::post('/ajax/vid', 'Larrock\ComponentCatalog\CatalogController@vid')->name('catalog.vid');
-});
+Route::get('/catalog', function(){ return Redirect::to('/'); });
+Route::get('/root', 'Larrock\ComponentCatalog\CatalogController@getCategoryRoot')->name('catalog.root');
+Route::get('/yml.xml', 'Larrock\ComponentCatalog\CatalogController@YML')->name('catalog.yml');
+Route::get('/catalog/all', 'Larrock\ComponentCatalog\CatalogController@getAllTovars')->name('catalog.all');
+Route::get('/catalog/{category}/{category2?}/{category3?}/{category4?}', 'Larrock\ComponentCatalog\CatalogController@getCategoryExpanded')->name('catalog.category');
+Route::any('/search/catalog/serp/{words?}', 'Larrock\ComponentCatalog\CatalogController@searchResult')->name('catalog.search.words');
+Route::get('/search/catalog', 'Larrock\ComponentCatalog\CatalogController@searchItem')->name('catalog.search');
+Route::post('/ajax/editPerPage', 'Larrock\ComponentCatalog\CatalogController@editPerPage')->name('catalog.editPerPage');
+Route::post('/ajax/sort', 'Larrock\ComponentCatalog\CatalogController@sort')->name('catalog.sort');
+Route::post('/ajax/vid', 'Larrock\ComponentCatalog\CatalogController@vid')->name('catalog.vid');
 
 Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu', 'SaveAdminPluginsData', 'SiteSearchAdmin']], function(){
     Route::resource('catalog', 'Larrock\ComponentCatalog\AdminCatalogController', ['names' => [
